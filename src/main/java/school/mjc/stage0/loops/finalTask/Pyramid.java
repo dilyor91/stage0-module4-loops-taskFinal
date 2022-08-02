c770024371313a3c810248597dfad2bef4a7603a
dilyor91 <dilyor_05@mail.ru>
2022-08-02 15:03:29 +0500

    Final Task v2

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int row = 0; row < cathetusLength; row++) {
+            int numIs = row + 1;
+            for (int col = 1; col <= cathetusLength + row; col++) {
+                if (col == cathetusLength - row || (col >= cathetusLength - row && col <= cathetusLength + col)) {
+                    System.out.print(numIs);
+                    if (numIs == 1 || col >= cathetusLength) {
+                        numIs++;
+                    } else {
+                        numIs--;
+                    }
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
